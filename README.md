f78e072fecec064011c5573e5e3fdb412f963f0c
flxc3r <felix.carrier@gmail.com>
2019-10-05 20:59:30 -0400

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# No Script No Style
+
+Get a webpage's content, without scripts or stylesheets.
+
+- App: python flask, urllib3, beautifulsoup4
+- Deployment: zappa (runs on AWS Lambda)
---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+import urllib3
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+# extract data
+def no_script_no_style(url):
+    http = urllib3.PoolManager()
+    response = http.request('GET', url)
+    soup = BeautifulSoup(response.data, 'html.parser')
+
+    [s.extract() for s in soup('script')]
+    [s.extract() for s in soup('style')]
+    return soup
+
+
+# render page
+@app.route('/', methods=['GET', "POST"])
+def index():
+    # POST
+    if request.method == 'POST':
+        url = request.form['url']
+        return redirect(url_for('index', url=url))
+
+    # GET
+    url = request.args.get('url')
+    if url is None or len(url)==0:
+        return render_template('index.html', url=None, content=None)
+    else:
+        soup = no_script_no_style(url)
+        return render_template('index.html', url=url, content=soup)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
